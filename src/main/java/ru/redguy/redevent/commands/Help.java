79bb614066bed40d0a291599fe64298560e15d0c
Ilya <admin@redguy.ru>
2021-07-13 02:41:52 +0300

    Help improve

--- src/main/java/ru/redguy/redevent/commands/Help.java
+++ src/main/java/ru/redguy/redevent/commands/Help.java
@@ -6,7 +6,31 @@ import org.bukkit.command.CommandSender;
 public class Help implements SubCommand {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        sender.sendMessage("/event [help/start/add/info/stop]");
+        if(args.length == 0) {
+            sender.sendMessage("/event help [subcommand]");
+            return true;
+        }
+        switch (args[0]) {
+            case "help":
+                sender.sendMessage("/event help [subcommand]");
+                break;
+            case "add":
+                sender.sendMessage("/event add [EventName]");
+                break;
+            case "start":
+                sender.sendMessage("/event start");
+                break;
+            case "i":
+            case "info":
+                sender.sendMessage("/event info");
+                break;
+            case "stop":
+                sender.sendMessage("/event stop");
+                break;
+            default:
+                sender.sendMessage("Unknown subcommand!");
+                break;
+        }
         return true;
     }
 }
